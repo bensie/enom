fd5024c66ce391b1a3e21106d0d47dd0a5d4d5b5
James Miller <bensie@gmail.com>
2011-01-29 14:55:23 -0800

    Get and set each of the contact types

--- lib/enom/contact_info.rb
+++ lib/enom/contact_info.rb
@@ -25,29 +25,37 @@ module Enom
       # ...
       # end
       define_method "#{contact_type.downcase}_contact_info" do
-        raise NotImplementedError
+        response = Client.request({'Command' => 'GetContacts', 'SLD' => sld, 'TLD' => tld}.merge(opts))["interface_response"]["GetContacts"][contact_type]
       end
 
       # Define setter methods for each contact type
       # def update_registrant_contact_info
       # ...
       # end
-      define_method "update_#{contact_type.downcase}_contact_info" do |data = {}|
+      define_method "update_#{contact_type.downcase}_contact_info" do |contact_data = {}|
 
         # Remove attributes that are not in Enom's list of available fields
-        data.select!{|k| FIELDS.map{|f| f[:name] }.include?(k)}
+        contact_data.select!{|k| FIELDS.map{|f| f[:name] }.include?(k)}
+
+        # Write the initial options hash containing the current ContactType
+        opts = {"ContactType" => contact_type}
 
         # Check to make sure all required fields are present
         FIELDS.each do |field|
           if field[:required]
-            if data[field[:name]].nil? || data[field[:name]].empty?
+            if contact_data[field[:name]].nil? || contact_data[field[:name]].empty?
               raise Error, "#{field[:name]} is required to update contact info"
             end
           end
         end
 
-        # response = Client.request('Command' => 'Contacts', 'SLD' => sld, 'TLD' => tld)["interface_response"]["Contacts"]
-        raise NotImplementedError
+        # Prepend ContactType to beginning of all data and add to options hash
+        contact_data.each do |k,v|
+          opts.merge!("#{contact_type}#{k}" => v)
+        end
+
+        response = Client.request({'Command' => 'Contacts', 'SLD' => sld, 'TLD' => tld}.merge(opts))["interface_response"]
+        return response["Done"] == "true"
       end
 
       # Update all contact types with the same data
