d11f7f10d1a5381d87be0084c1fadc0f488b19c8
Tsvetan Roustchev <tsv@hitsol.net>
2013-04-10 18:38:18 +0300

    Enom::Certificate removed

--- lib/enom.rb
+++ lib/enom.rb
@@ -3,7 +3,6 @@ require 'httparty'
 require File.expand_path('../enom/client',  __FILE__)
 require File.expand_path('../enom/contact_info',   __FILE__)
 require File.expand_path('../enom/domain',  __FILE__)
-require File.expand_path('../enom/certificate',   __FILE__)
 require File.expand_path('../enom/account', __FILE__)
 require File.expand_path('../enom/error',   __FILE__)
 
